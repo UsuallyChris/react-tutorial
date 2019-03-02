7c74712fdf763f503f811c412816bd5bfc802665
UsuallyChris <schoonc@gmail.com>
2019-03-01 18:58:17 -0800

    Added Form.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,7 @@ import Table from './Table.js';
 
 class App extends Component {
   state = {
-    characters: [
-      { 'name': 'Charlie',
-        'job': 'Janitor'
-      },
-      {
-        'name': 'Mac',
-        'job': 'Bouncer'
-      },
-      {
-        'name': 'Dee',
-        'job': 'Aspiring Actress'
-      },
-      {
-        'name': 'Dennis',
-        'job': 'Bartender'
-      }
-    ]
+    characters: []
   };
 
   removeCharacter = index => {
